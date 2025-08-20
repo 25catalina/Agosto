84a65f660631c8e87b53476e30c59989fc454b91
catalina <catalina.caripan.lonconanco@alumnos.sip.cl>
2025-08-20 10:45:21 -0400

    version1

---
+++ ejemplo.js
@@ -0,0 +1,8 @@
+
+let edad = 18;
+let rcm = 120 - edad * 0.7;
+let nombre = catalina
+
+console.log(edad)
+console.log(rcm)
+console.log(nombre)
---
+++ script.js
@@ -0,0 +1,6 @@
+let edad = 18;
+let nombre = "catalina";
+let estatura = 1.46;
+let apellido = "Caripan";
+let anoActual = 2025;
+let anoNacimiento = anoActual - edad;
